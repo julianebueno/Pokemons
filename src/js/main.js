fda5ed0f891a18c3c126eeaf85c9d2bd5a29b3cf
Juliane Bueno <julianebueno1@gmail.com>
2024-01-25 07:02:07 -0300

    feat: :zap: reestrutura da função da API e adicionado navegação

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,58 @@
+async function pegarPokemonAPI(inicio, quantiaPokemon) {
+  let msg = document.createElement("p");
+  msg.innerHTML = "Carregando...";
+  albumPokemon.append(msg);
 
-async function criarCard() {
-    console.log("entrouPokemon")
-    let quantiaPokemon = 10
-    
-    for (let i = 1; i <= quantiaPokemon; i++) {
+  let arrayPokemon = [];
+  for (let i = inicio; i <= quantiaPokemon; i++) {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const data = response.data;
+    data.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${i}.png`;
+    arrayPokemon.push(data);
+  }
 
-        console.log("entrouFor")
-        let card = document.createElement("div");
-        card.classList.add('cardPokemon');
-
-        let imagePokemon = document.createElement("img");
-        imagePokemon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${i}.png`
-
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
-        const data = response.data;
-
-        card.append(data.id);
-        card.append(imagePokemon);
-        card.append(data.name);
-        
-        albumPokemon.append(card)
-        
-    }
-    
+  albumPokemon.innerHTML = "";
+  criarCard(arrayPokemon);
 }
 
-let albumPokemon = document.getElementById('albumPokemon')
-criarCard()
+function criarCard(arrayPokemon) {
+  for (let i = 0; i < arrayPokemon.length; i++) {
+    let card = document.createElement("div");
+    card.classList.add("cardPokemon");
+
+    let idPokemon = document.createElement("small");
+    idPokemon.innerHTML = arrayPokemon[i].id;
+    let imagePokemon = document.createElement("img");
+    imagePokemon.src = arrayPokemon[i].img;
+    let namePokemon = document.createElement("p");
+    namePokemon.innerHTML =
+      arrayPokemon[i].name.charAt(0).toUpperCase() +
+      arrayPokemon[i].name.slice(1);
+
+    card.append(idPokemon);
+    card.append(imagePokemon);
+    card.append(namePokemon);
+    albumPokemon.append(card);
+  }
+}
+
+function voltarPagina() {
+  if (ini > 1) {
+    ini -= 30;
+    quantPokemon -= 30;
+    albumPokemon.innerHTML = "";
+    pegarPokemonAPI(ini, quantPokemon);
+  }
+}
+
+function avancarPagina() {
+  ini += 30;
+  quantPokemon += 30;
+  albumPokemon.innerHTML = "";
+  pegarPokemonAPI(ini, quantPokemon);
+}
+
+let albumPokemon = document.getElementById("albumPokemon");
+let ini = 1;
+let quantPokemon = 30;
+pegarPokemonAPI(ini, quantPokemon);
